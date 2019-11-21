f00ad11b11789879e206428ef23b852f66242f67
Uladzislau Danilchyk <uladzislau_danilchyk@epam.com>
2019-11-21 12:36:48 +0300

    Created main components

--- training-project/src/app/app.module.ts
+++ training-project/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CoursesComponent } from './courses/courses.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CoursesComponent,
+    BreadcrumbsComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule
